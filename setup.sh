4857461653d971caac9ecfc9467aca47a94534b8
Ioana Caracas <icaracas@dunegpvm15.fnal.gov>
2025-04-22 09:54:38 -0500

    try slightly different setup for the new token

--- setup.sh
+++ setup.sh
@@ -3,10 +3,11 @@ source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 setup cmake v3_9_0
 setup gcc v6_4_0
 setup eigen v3_3_5
+setup root v6_28_10b -q e20:p3915:prof
 setup python v2_7_3
 
 export PYTHONPATH=${PYTHONPATH}:${PWD}/lib/
 
 # Optional
 #setup root v6_12_06a -q e15:prof
-setup root v6_28_10b -q e20:p3915:prof
+#setup root v6_28_10b -q e20:p3915:prof
