54f064de58a62ed5a503f98bf5d1f3497dd48696
Ioana Caracas <icaracas@dunegpvm15.fnal.gov>
2025-04-23 02:17:09 -0500

    cmake3.18

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 
-setup cmake v3_9_0
+setup cmake v3_18_2
 setup gcc v6_4_0
 setup eigen v3_3_5
 setup root v6_28_10b -q e20:p3915:prof
@@ -11,6 +11,7 @@ export PYTHONPATH=${PYTHONPATH}:${PWD}/lib/
 which gcc
 which root
 which python
+cmake --version
 # Optional
 #setup root v6_12_06a -q e15:prof
 #setup root v6_28_10b -q e20:p3915:prof
