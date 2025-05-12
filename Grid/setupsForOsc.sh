465c1796c75933c3a366047c6b3e8e8888f4a049
Ioana Caracas <icaracas@dunegpvm15.fnal.gov>
2025-05-12 07:30:36 -0500

    modifications done for running the code with the oscillation probability on the fermi grid. the setupsForOsc.h is the script to be sourced and afer that just type make. then everything is fine and can run the code as usual

---
+++ Grid/setupsForOsc.sh
@@ -0,0 +1,8 @@
+source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+
+setup root v6_28_12 -q e26:prof:p3915
+setup boost v1_82_0 -qdebug:e26
+setup eigen v23_08_01_66e8f
+setup stan_math v4_9_0a
+setup sundials v7_1_1 
+setup osclib v00.27 -q e26:prof:n319:stanfree
